e687d288bdcfbee694abdfcfe61daa6ca37b42e1
Martin <martindeiss@gmail.com>
2016-03-28 23:29:33 +0200

    removed old code

--- src/config/routes/base/BaseRoutes.ts
+++ src/config/routes/base/BaseRoutes.ts
@@ -1,6 +1,4 @@
 import express = require("express");
-import HeroRoutes = require("./../HeroRoutes");
-import SpartanRoutes = require("./../SpartanRoutes");
 import UserRoutes = require("./../UserRoutes");
 
 var app = express();
@@ -8,8 +6,6 @@ var app = express();
 class BaseRoutes {
     
     get routes() {
-        app.use("/", new HeroRoutes().routes);
-        app.use("/", new SpartanRoutes().routes);
         app.use("/", new UserRoutes().routes);
         return app;
     }
